fab1f0ac8015312dc08811a0a4262ace3da9ec15
supah hakka <>
2023-06-14 16:15:13 +0200

    refactor variable names, style header and add basic element variables for js
    Decided to use camelCase for JavaScript file and changed all html/css variables to use hyphen instead of underscore to separate words.
    
    Changed margin and padding for header. Also made a vertical line class to better separate visual modules.
    
    Added element variables to the JavaScript file for input textarea, output textarea and quick start button.

---
+++ script.js
@@ -0,0 +1,9 @@
+textIn = document.getElementById('text-in')
+textOut = document.getElementById('text-out')
+quickStart = document.getElementById('quick-start')
+
+quickStart.addEventListener("click", quickGenerate)
+
+function quickGenerate() {
+    console.log('Calling Quick Generate.')
+}
